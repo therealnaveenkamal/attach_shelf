c18b5cdb86c282864411242ea95cfa4596279001
Naveen <therealnaveenkamal@gmail.com>
2023-11-22 18:23:33 +0000

    Corrected cart_frame TF issue

--- src/approach_service_server.cpp
+++ src/approach_service_server.cpp
@@ -18,7 +18,6 @@
 class ApproachServiceServer : public rclcpp::Node {
 public:
   ApproachServiceServer() : Node("approach_service_server") {
-
     auto clock = this->get_clock();
     tf_buffer = std::make_shared<tf2_ros::Buffer>(clock);
     tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
@@ -34,8 +33,7 @@ public:
         std::bind(&ApproachServiceServer::scan_callback, this,
                   std::placeholders::_1));
 
-    msgvalue.resize(NUM_LASER_READINGS,
-                    0.0); // Adjust the size based on your laser configuration
+    msgvalue.resize(NUM_LASER_READINGS, 0.0);
     cmd_vel_publisher_ =
         this->create_publisher<geometry_msgs::msg::Twist>("/robot/cmd_vel", 10);
     timer_ = this->create_wall_timer(
@@ -57,6 +55,8 @@ private:
   double angleLegs = 0.0;
   double xDistance = 0.0;
   double yDistance = 0.0;
+  double xDistance_once = 0.0;
+  double yDistance_once = 0.0;
   bool ready = false;
   bool move_extra = false;
   bool elevated = false;
@@ -73,7 +73,7 @@ private:
   std::shared_ptr<tf2_ros::TransformBroadcaster> broadcaster;
   rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr elevator_publisher;
 
-  bool handle_approach_request(
+  void handle_approach_request(
       const std::shared_ptr<attach_shelf::srv::GoToLoading::Request> request,
       std::shared_ptr<attach_shelf::srv::GoToLoading::Response> response) {
 
@@ -97,8 +97,6 @@ private:
     } else {
       response->complete = false;
     }
-
-    return true;
   }
 
   void timer_callback() {
@@ -124,53 +122,131 @@ private:
     }
   }
 
-   void publish_cart_frame_transform() {
+/*
+void publish_cart_frame_transform() {
+    geometry_msgs::msg::TransformStamped transformStamped;
+    geometry_msgs::msg::Twist cmd_vel_msg;
 
-        geometry_msgs::msg::TransformStamped transformStamped;
-      geometry_msgs::msg::Twist cmd_vel_msg;
+      if (ready) {
 
       try {
+        // Get the fixed transform from robot_odom to front_laser_link
         transformStamped = tf_buffer->lookupTransform(
-            "robot_odom", "robot_front_laser_base_link", tf2::TimePointZero);
-      } catch (tf2::TransformException &ex) {
+            "robot_odom", "robot_base_link", tf2::TimePointZero);
+    } catch (tf2::TransformException &ex) {
         RCLCPP_WARN(this->get_logger(), "Could not transform %s to %s",
                     "robot_odom", "robot_front_laser_base_link");
         return; // Return early in case of a transform exception
+    }
+
+    // Calculate the fixed distance between robot_odom and front_laser_link
+    tf2::Vector3 odom_to_laser_distance(
+        transformStamped.transform.translation.x,
+        transformStamped.transform.translation.y,
+        transformStamped.transform.translation.z);
+
+    // Recalculate the transformed coordinates inside the loop
+    tf2::Vector3 base_coords(xDistance, yDistance, 0.0);
+
+    tf2::Vector3 base_coords_var(yDistance, xDistance, 0.0);
+
+    // Calculate the updated position of cart_frame with respect to robot_odom
+    tf2::Vector3 cart_frame_coords = odom_to_laser_distance + base_coords;
+
+    // Broadcast the transform for "cart_frame"
+    transform_stamped.header.stamp = transformStamped.header.stamp;
+    transform_stamped.header.frame_id = "robot_odom";
+    transform_stamped.child_frame_id = "cart_frame";
+    transform_stamped.transform.translation.x = xDistance;
+    transform_stamped.transform.translation.y = yDistance;
+    transform_stamped.transform.translation.z = 0.0;
+    transform_stamped.transform.rotation.x = cart_quat_.x();
+    transform_stamped.transform.rotation.y = cart_quat_.y();
+    transform_stamped.transform.rotation.z = cart_quat_.z();
+    transform_stamped.transform.rotation.w = cart_quat_.w();
+    broadcaster->sendTransform(transform_stamped);
+    
+        // Calculate the distance between cart_frame and front_laser_link
+        tf2::Vector3 front_laser_coords = odom_to_laser_distance + base_coords_var;
+
+        double tfDistance = sqrt(pow(xDistance, 2) + pow(yDistance, 2));
+
+        double distance_to_target = base_coords_var.length();
+
+        RCLCPP_INFO(this->get_logger(), "Distance to front_laser_link: %f", tfDistance);
+        if (tfDistance >= 0.4) {
+          cmd_vel_msg.linear.x = 0.1;
+          cmd_vel_msg.angular.z = 0.0;
+          cmd_vel_publisher_->publish(cmd_vel_msg);
+        } else {
+          cmd_vel_msg.linear.x = 0.0;
+          cmd_vel_msg.linear.y = 0.0;
+          cmd_vel_msg.angular.z = 0.0;
+          cmd_vel_publisher_->publish(cmd_vel_msg);
+
+          ready = false;
+          move_extra = true;
+        }
       }
 
-      // Recalculate the transformed coordinates inside the loop
-      tf2::Vector3 transformed_coords(
-          transformStamped.transform.translation.x + xDistance,
-          transformStamped.transform.translation.y + yDistance,
-          transformStamped.transform.translation.z);
+} */
 
-      double distance_to_target = transformed_coords.length();
+void publish_cart_frame_transform() {
+  geometry_msgs::msg::Twist cmd_vel_msg;
 
-      // Broadcast the transform for "cart_frame"
-      transform_stamped.header.stamp = transformStamped.header.stamp;
-      transform_stamped.header.frame_id = "robot_front_laser_base_link";
-      transform_stamped.child_frame_id = "cart_frame";
-      transform_stamped.transform.translation.x = xDistance;
-      transform_stamped.transform.translation.y = yDistance;
-      transform_stamped.transform.translation.z = 0.0;
-      transform_stamped.transform.rotation.x = cart_quat_.x();
-      transform_stamped.transform.rotation.y = cart_quat_.y();
-      transform_stamped.transform.rotation.z = cart_quat_.z();
-      transform_stamped.transform.rotation.w = cart_quat_.w();
-      broadcaster->sendTransform(transform_stamped);
+  try {
+    auto odom_to_base_transform = tf_buffer->lookupTransform(
+        "robot_odom", "robot_base_link", tf2::TimePointZero);
+    auto base_to_laser_transform = tf_buffer->lookupTransform(
+        "robot_base_link", "robot_front_laser_base_link", tf2::TimePointZero);
+    tf2::Vector3 base_coords(xDistance, yDistance, 0.0);
+    tf2::Quaternion laser_quat;
+    tf2::fromMsg(base_to_laser_transform.transform.rotation, laser_quat);
+
+    tf2::Vector3 laser_coords = tf2::quatRotate(laser_quat, base_coords);
+
+    tf2::Quaternion odom_quat;
+    tf2::fromMsg(odom_to_base_transform.transform.rotation, odom_quat);
+
+    tf2::Vector3 odom_coords =
+        tf2::quatRotate(odom_quat, laser_coords);
+    odom_coords += tf2::Vector3(odom_to_base_transform.transform.translation.x,
+                                odom_to_base_transform.transform.translation.y,
+                                odom_to_base_transform.transform.translation.z);
+
 
     if (ready) {
 
-      tf2::Vector3 direction_to_target = transformed_coords.normalized();
-      float tfDistance = sqrt(pow(xDistance, 2) +
-                              pow(yDistance, 2));
-      RCLCPP_INFO(this->get_logger(), "DIST: %f", tfDistance);
+    if(xDistance_once==0.0){
+         xDistance_once = odom_coords.x();
+    }
+
+    if(yDistance_once==0.0){
+         yDistance_once = odom_coords.y();
+    }
+
+    transform_stamped.header.stamp = odom_to_base_transform.header.stamp;
+    transform_stamped.header.frame_id = "robot_odom";
+    transform_stamped.child_frame_id = "cart_frame";
+    transform_stamped.transform.translation.x = xDistance_once;
+    transform_stamped.transform.translation.y = yDistance_once;
+    transform_stamped.transform.translation.z = odom_coords.z();
+    transform_stamped.transform.rotation.x = cart_quat_.x();
+    transform_stamped.transform.rotation.y = cart_quat_.y();
+    transform_stamped.transform.rotation.z = cart_quat_.z();
+    transform_stamped.transform.rotation.w = cart_quat_.w();
+    broadcaster->sendTransform(transform_stamped);
+
+        double tfDistance = sqrt(pow(xDistance, 2) + pow(yDistance, 2));
+        RCLCPP_INFO(this->get_logger(), "Dist: %f", tfDistance);
 
       if (tfDistance >= 0.4) {
         cmd_vel_msg.linear.x = 0.1;
         cmd_vel_msg.angular.z = 0.0;
         cmd_vel_publisher_->publish(cmd_vel_msg);
       } else {
+        xDistance_once = odom_coords.x();
+        yDistance_once = odom_coords.y();
         cmd_vel_msg.linear.x = 0.0;
         cmd_vel_msg.linear.y = 0.0;
         cmd_vel_msg.angular.z = 0.0;
@@ -179,13 +255,21 @@ private:
         ready = false;
         move_extra = true;
       }
+    }
+    else if (ready == false && move_extra == true){
+
+   
 
     }
+
+  } catch (tf2::TransformException &ex) {
+    RCLCPP_WARN(this->get_logger(), "Transform exception: %s", ex.what());
+    return;
   }
+}
 
 
   void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
-
     for (size_t i = 0; i < msg->intensities.size(); ++i) {
       msgvalue[i] = msg->ranges[i];
     }
@@ -196,10 +280,7 @@ private:
 
     float tempIntensity = 0.0;
 
-    // RCLCPP_INFO(this->get_logger(), "------%d", msg->intensities.size());
-
     for (int i = 1; i < msg->intensities.size() - 1; ++i) {
-
       if (msg->intensities[i] > threshold &&
           msg->intensities[i + 1] > threshold) {
         tempIntensity = 0.0;
@@ -209,15 +290,11 @@ private:
           }
           if (tempIntensity >= 60000) {
             legPositions.push_back(i + 5);
-            // RCLCPP_INFO(this->get_logger(), "POS: %d, %f", i +
-            // 5,tempIntensity);
             i = j;
           }
         }
       }
     }
-
-    // RCLCPP_INFO(this->get_logger(), "------");
 
     globalLegPos = legPositions;
 
